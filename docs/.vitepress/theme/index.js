b3b0746e09f1c7b82bfe289d5a5918106fb635cd
sixlive <tj@tjmiller.co>
2025-04-19 11:26:28 -0400

    docs: new theme

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,7 @@
 import { h } from "vue";
 import DefaultTheme from "vitepress/theme";
 import HostedFooter from "./HostedFooter.vue";
+import "./custom.css";
 
 export default {
   extends: DefaultTheme,
